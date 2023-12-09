4b4dfb8903f3f28113702acf9cd7e99ec92c01b3
Ozod <o.bukhorizoda@mail.ru>
2023-12-09 12:27:02 +0500

    issue with loader

--- src/components/loader/Loader.jsx
+++ src/components/loader/Loader.jsx
@@ -2,7 +2,7 @@ import styles from "./Loader.module.css";
 export const Loader = () => {
   return (
     <div className={styles.loaderContainer}>
-      <img className={styles.loader} src="public/rps.gif" alt="Coffee" />
+      <img className={styles.loader} src="rps.gif" alt="Scissors" />
       <h3>Loading...</h3>
     </div>
   );
